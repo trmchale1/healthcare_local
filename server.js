d911ad98c8fc7356ab4311b2bd444f4675586ebf
root <trmchale@gmail.com>
2017-04-01 03:25:23 -0400

    working

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var parse = require('csv-parse');
+
+fs.readFile('states.csv', 'utf8', function(err,data){
+	if(err){
+		return console.log(err);
+	}
+	var content = data;
+	console.log(content);
+});
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html')
-})
+	res.sendFile(__dirname + '/index.html');
+	res.send('hello world');
+});
 
 app.listen(3000, function () {
   console.log('Running.')
-})
+});
